8da9956c3230c6fd94285c539ca5256be5e31cd0
Oliver Butterley <oliver.butterley@gmail.com>
2025-11-04 11:12:34 +0100

    verify from_bytes_mod_order_spec but with lemma required

--- Curve25519Dalek/Specs/Scalar/Scalar/FromBytesModOrder.lean
+++ Curve25519Dalek/Specs/Scalar/Scalar/FromBytesModOrder.lean
@@ -5,6 +5,7 @@ Authors: Oliver Butterley, Markus Dablander
 -/
 import Curve25519Dalek.Funs
 import Curve25519Dalek.Defs
+import Curve25519Dalek.Specs.Scalar.Scalar.Reduce
 
 /-! # Spec Theorem for `Scalar::from_bytes_mod_order`
 
@@ -34,17 +35,25 @@ natural language specs:
     • scalar_to_nat(s) < \ell
 -/
 
+/-- If the natural number represented by a byte array is less than L, then the high bit (bit 7 of
+byte 31) must be 0. This is because L < 2^255. -/
+theorem high_bit_zero_of_lt_L (bytes : Array U8 32#usize) (h : U8x32_as_Nat bytes < L) :
+    bytes.val[31]!.val >>> 7 = 0 := by
+  sorry
+
 /-- **Spec and proof concerning `scalar.Scalar.from_bytes_mod_order`**:
 - No panic (always returns successfully)
 - The result scalar s, when converted to nat, equals the input bytes converted to nat modulo L
-- The result scalar s is less than L (the group order)
--/
+- The result scalar s is less than L (the group order) -/
+@[progress]
 theorem from_bytes_mod_order_spec (b : Array U8 32#usize) :
-    ∃ s,
-    from_bytes_mod_order b = ok s ∧
+    ∃ s, from_bytes_mod_order b = ok s ∧
     U8x32_as_Nat s.bytes ≡ U8x32_as_Nat b [MOD L] ∧
-    U8x32_as_Nat s.bytes < L
-    := by
-  sorry
+    U8x32_as_Nat s.bytes < L := by
+  unfold from_bytes_mod_order scalar.Indexcurve25519_dalekscalarScalarUsizeU8.index
+  progress*
+  have := high_bit_zero_of_lt_L s.bytes
+  simp [*] at *
+  grind
 
 end curve25519_dalek.scalar.Scalar
