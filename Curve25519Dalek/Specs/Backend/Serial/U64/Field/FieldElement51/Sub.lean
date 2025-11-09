17b00fbf625d2382d3111bb2351fd7f86488fe97
hoangtruong <truonghoangle@gmail.com>
2025-11-10 05:21:37 +0700

    Update Sub.lean, status

--- Curve25519Dalek/Specs/Backend/Serial/U64/Field/FieldElement51/Sub.lean
+++ Curve25519Dalek/Specs/Backend/Serial/U64/Field/FieldElement51/Sub.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Beneficial AI Foundation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Markus Dablander
+Authors: Markus Dablander, Hoang Le Truong
 -/
 import Curve25519Dalek.Funs
 import Curve25519Dalek.Defs
@@ -17,8 +17,6 @@ of p is added.
 
 **Source**: curve25519-dalek/src/backend/serial/u64/field.rs
 
-## TODO
-- Complete proof
 -/
 
 open Aeneas.Std Result
