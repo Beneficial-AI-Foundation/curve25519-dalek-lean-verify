b366bae869aeeb0f955287b9ed931731dc7cf2be
Oliver Butterley <oliver.butterley@gmail.com>
2025-11-09 18:21:02 +0100

    rustflags

--- scripts/build-rust-docs.sh
+++ scripts/build-rust-docs.sh
@@ -5,12 +5,20 @@
 
 set -e
 
-RUSTDOCFLAGS='
+# Set both RUSTFLAGS (for compilation) and RUSTDOCFLAGS (for documentation)
+# The backend flags must be in both to ensure vector backend files aren't compiled
+export RUSTFLAGS='
+  --cfg curve25519_dalek_backend="serial"
+  --cfg curve25519_dalek_bits="64"
+'
+
+export RUSTDOCFLAGS='
   --cfg docsrs
   --cfg curve25519_dalek_backend="serial"
   --cfg curve25519_dalek_bits="64"
   --html-in-header curve25519-dalek/docs/assets/rustdoc-include-katex-header.html
-' \
+'
+
 cargo +nightly-2025-07-20 rustdoc \
   -p curve25519-dalek
 
