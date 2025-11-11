c0864236ffe1c25403b442226da182595dd31ce8
hoangtruong <truonghoangle@gmail.com>
2025-11-11 19:09:34 +0700

    complete is-zero-proof

--- Curve25519Dalek/Specs/Field/FieldElement51/IsZero.lean
+++ Curve25519Dalek/Specs/Field/FieldElement51/IsZero.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Beneficial AI Foundation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Markus Dablander
+Authors: Markus Dablander, Hoang Le Truong
 -/
 import Curve25519Dalek.Funs
 import Curve25519Dalek.Defs
@@ -21,8 +21,7 @@ This function checks whether a field element is zero.
 
 **Source**: curve25519-dalek/src/field.rs
 
-## TODO
-- Complete proof
+
 -/
 
 open Aeneas.Std Result
@@ -54,9 +53,6 @@ lemma Choice.val_eq_one_iff (c : subtle.Choice) : c.val = 1#u8 ↔ c = Choice.on
   | mk val valid =>
     simp [Choice.one]
 
-lemma to_bytes_ok_extract (r : backend.serial.u64.field.FieldElement51) (h1 : Aeneas.Std.Array U8 32#usize)
-    (h2 : r.to_bytes = ok h1) : ∃ v,  r.to_bytes = ok v ∧ v = h1 := by
-  exists h1
 
 
 lemma array_eq_of_to_slice_eq
@@ -75,14 +71,7 @@ lemma array_eq_of_to_slice_eq
 
 
 
-@[simp]
-theorem UScalar.ofNat_inj {a b : U8} (h : ↑a = ↑b) : a = b := by
-  cases a; cases b
-  simp_all
 
-theorem Array.val_getElem!_eq'_U8 (bs : Array U8 32#usize) (i : Nat) (h : i < bs.length) :
-    (bs.val)[i]! = bs[i] := by
-  simpa [Subtype.val] using getElem!_pos bs.val i _
 
 
 
@@ -92,54 +81,52 @@ theorem is_zero_spec (r : backend.serial.u64.field.FieldElement51) :
     ∃ c, is_zero r = ok c ∧
     (c.val = 1#u8 ↔ Field51_as_Nat r % p = 0)
     := by
-    unfold is_zero
-    progress as ⟨bytes, h_to_bytes⟩
-    progress as ⟨s, h_bytes_slice⟩
-    progress as ⟨s1, h_zero_slice⟩
-    progress as ⟨result, h_ct_eq⟩
-    constructor
-    . intro h
-      rename_i h1
-      have s_eq_s1 : s = s1 := by exact h_ct_eq.mp ((Choice.val_eq_one_iff result).mp h)
-      rw[h_bytes_slice ,h_zero_slice ] at s_eq_s1
-      have heq: bytes = (Array.repeat 32#usize 0#u8) := by
-        apply array_eq_of_to_slice_eq
-        exact s_eq_s1
-      rw[heq,Nat.ModEq] at h_to_bytes
-      rw[← h_to_bytes]
-      unfold U8x32_as_Nat
+  unfold is_zero
+  progress as ⟨bytes, h_to_bytes⟩
+  progress as ⟨s, h_bytes_slice⟩
+  progress as ⟨s1, h_zero_slice⟩
+  progress as ⟨result, h_ct_eq⟩
 
-      iterate 31 (rw [Finset.sum_range_succ])
-      rw[Finset.sum_range_one]
-      simp[ Array.repeat]
+  constructor
+  · intro h
+    have s_eq_s1 : s = s1 := by
+      exact h_ct_eq.mp ((Choice.val_eq_one_iff result).mp h)
+    rw [h_bytes_slice, h_zero_slice] at s_eq_s1
+    have heq : bytes = Array.repeat 32#usize 0#u8 := by
+      apply array_eq_of_to_slice_eq
+      exact s_eq_s1
+    rw [heq, Nat.ModEq] at h_to_bytes
+    rw [← h_to_bytes]
+    unfold U8x32_as_Nat
+    iterate 31 (rw [Finset.sum_range_succ])
+    rw [Finset.sum_range_one]
+    simp [Array.repeat]
 
-    intro h
+  · intro h
     rename_i h1
-    rw[Nat.ModEq,h] at h_to_bytes
+    rw [Nat.ModEq, h] at h_to_bytes
     have h_bytes_zero : U8x32_as_Nat bytes = 0 := by
       have h2 := Nat.mod_eq_of_lt h1
       rw [h2] at h_to_bytes
       exact h_to_bytes
 
-
-    have by_eq1 :bytes= (Array.repeat 32#usize 0#u8) := by
+    have bytes_eq : bytes = Array.repeat 32#usize 0#u8 := by
       unfold U8x32_as_Nat at h_bytes_zero
       simp_all
       apply Subtype.ext
       apply List.ext_getElem
-      simp
-      simp
-      intro i h₁ h₂
-      have h:= h_bytes_zero i h₁
-      have hval : ((Array.repeat 32#usize 0#u8).val)[i] = (0#u8 : U8) := by
-         sorry
-      rw[hval]
-      sorry
+      repeat simp
+      intro i hi _
+      have hi_val := h_bytes_zero i hi
+      -----
+      interval_cases i
+      all_goals simp [Array.repeat, List.replicate]; scalar_tac
+      -----
 
-    have   s_eq_s1 : s = s1 := by
-      rw[h_bytes_slice,h_zero_slice,by_eq1]
-    rw[← h_ct_eq] at s_eq_s1
-    simp[s_eq_s1,Choice.one]
+    have s_eq_s1 : s = s1 := by
+      rw [h_bytes_slice, h_zero_slice, bytes_eq]
+    rw [← h_ct_eq] at s_eq_s1
+    simp [s_eq_s1, Choice.one]
 
 
 
