9f1560a0876591e37baa90bd298e3e84721bb509
Markus Ferdinand Dablander <25180655+MarkusFerdinandDablander@users.noreply.github.com>
2025-11-25 12:34:47 +0100

    Add file for Mul.mul spec thm (#217)

--- Curve25519Dalek.lean
+++ Curve25519Dalek.lean
@@ -23,6 +23,7 @@ import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.AsBytes
 import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.FromBytes
 import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.FromLimbs
 import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.MINUS_ONE
+import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.Mul
 import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.Negate
 import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.ONE
 import Curve25519Dalek.Specs.Backend.Serial.U64.Field.FieldElement51.Pow2K
