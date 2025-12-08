5908ef345537008bffe0cdde3baef5e0fbf673ae
Zhang-Liao <zhangliao714@gmail.com>
2025-12-08 23:06:36 +0800

    minor

--- Curve25519Dalek/Specs/Backend/Serial/U64/Scalar/Scalar52/MontgomerySquare.lean
+++ Curve25519Dalek/Specs/Backend/Serial/U64/Scalar/Scalar52/MontgomerySquare.lean
@@ -42,7 +42,7 @@ natural language specs:
   (m * m) ≡ w * R (mod L), where R = 2^260 is the Montgomery constant
 -/
 @[progress]
-theorem montgomery_square_spec (m : Scalar52)  (hm : ∀ i < 5, m[i]!.val < 2 ^ 62):
+theorem montgomery_square_spec (m : Scalar52) (hm : ∀ i < 5, m[i]!.val < 2 ^ 62):
     ∃ w,
     montgomery_square m = ok w ∧
     (Scalar52_as_Nat m * Scalar52_as_Nat m) % L = (Scalar52_as_Nat w * R) % L
