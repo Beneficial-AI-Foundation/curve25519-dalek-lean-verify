8f93a56448290c4c2ffb3941e523ad44a8a00328
a-dangelo <dangelo.alessand@gmail.com>
2025-12-28 14:44:22 +0000

    refactor(scalar52): update MulInternal spec and MontgomeryMul proof

--- Curve25519Dalek/Specs/Backend/Serial/U64/Scalar/Scalar52/MulInternal.lean
+++ Curve25519Dalek/Specs/Backend/Serial/U64/Scalar/Scalar52/MulInternal.lean
@@ -7,7 +7,7 @@ import Curve25519Dalek.Specs.Backend.Serial.U64.Scalar.M
 
 set_option linter.style.longLine false
 set_option linter.style.setOption false
-set_option maxHeartbeats 3000000
+set_option maxHeartbeats 4000000 -- scalr_tac exceeds standard heartbeats rates
 set_option exponentiation.threshold 500
 
 /-! # MulInternal
@@ -34,7 +34,8 @@ attribute [-simp] Int.reducePow Nat.reducePow
 theorem mul_internal_spec (a b : Array U64 5#usize)
     (ha : ∀ i < 5, a[i]!.val < 2 ^ 62) (hb : ∀ i < 5, b[i]!.val < 2 ^ 62) :
     ∃ result, mul_internal a b = ok (result) ∧
-    Scalar52_wide_as_Nat result = Scalar52_as_Nat a * Scalar52_as_Nat b := by
+    Scalar52_wide_as_Nat result = Scalar52_as_Nat a * Scalar52_as_Nat b ∧
+    (∀ i < 9, result[i]!.val < 2 ^ 127) := by
   unfold mul_internal
   unfold backend.serial.u64.scalar.Indexcurve25519_dalekbackendserialu64scalarScalar52UsizeU64.index
   progress*
@@ -88,8 +89,72 @@ theorem mul_internal_spec (a b : Array U64 5#usize)
     -- END TASK
   · -- BEGIN TASK
     simp [*, Scalar52_wide_as_Nat, Scalar52_as_Nat, Finset.sum_range_succ]
-    grind
+    refine ⟨?_,?_⟩
+    · -- Main equation
+      try grind
+    · -- Post condition
+      intro i hi
+      expand ha with 5
+      expand hb with 5
+
+      interval_cases i
+      · -- subgoal 1
+        simp only [Nat.not_eq, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, OfNat.zero_ne_ofNat,
+          not_lt_zero', Nat.ofNat_pos, or_true, or_self, List.getElem!_set_ne, one_ne_zero,
+          zero_ne_one, zero_lt_one, List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set,
+          gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 2
+        simp only [Nat.not_eq, ne_eq, OfNat.ofNat_ne_one, not_false_eq_true, OfNat.one_ne_ofNat,
+          Nat.not_ofNat_lt_one, Nat.one_lt_ofNat, or_true, or_self, List.getElem!_set_ne,
+          List.length_set, List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set,
+          gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 3
+        simp only [Nat.not_eq, ne_eq, Nat.reduceEqDiff, not_false_eq_true, Nat.reduceLT, or_true,
+          or_self, List.getElem!_set_ne, Nat.succ_ne_self, Nat.lt_add_one, List.length_set,
+          List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 4
+        simp only [Nat.not_eq, ne_eq, Nat.reduceEqDiff, not_false_eq_true, Nat.reduceLT, or_true,
+          or_self, List.getElem!_set_ne, Nat.succ_ne_self, Nat.lt_add_one, List.length_set,
+          List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 5
+        simp only [Nat.not_eq, ne_eq, Nat.reduceEqDiff, not_false_eq_true, Nat.reduceLT, or_true,
+          or_self, List.getElem!_set_ne, Nat.succ_ne_self, Nat.lt_add_one, List.length_set,
+          List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 6
+        simp only [Nat.not_eq, ne_eq, Nat.reduceEqDiff, not_false_eq_true, Nat.reduceLT, or_true,
+          or_self, List.getElem!_set_ne, Nat.succ_ne_self, Nat.lt_add_one, List.length_set,
+          List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 7
+        simp only [Nat.not_eq, ne_eq, Nat.reduceEqDiff, not_false_eq_true, Nat.reduceLT, or_true,
+          or_self, List.getElem!_set_ne, Nat.succ_ne_self, Nat.lt_add_one, List.length_set,
+          List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 8
+        simp only [Nat.not_eq, ne_eq, Nat.succ_ne_self, not_false_eq_true, Nat.reduceEqDiff,
+          Nat.reduceLT, Nat.lt_add_one, or_true, or_self, List.getElem!_set_ne, List.length_set,
+          List.Vector.length_val, UScalar.ofNat_val_eq, List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
+      · -- subgoal 9
+        simp only [List.length_set, List.Vector.length_val, UScalar.ofNat_val_eq, Nat.lt_add_one,
+          List.getElem!_set, gt_iff_lt]
+        simp only [*]
+        try scalar_tac
     -- END TASK
+
 
 
 end curve25519_dalek.backend.serial.u64.scalar.Scalar52
