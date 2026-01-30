7b9834b00b143d37cbb57f0d4f34f496c5e732f0
Markus Dablander <markus.dablander@gmx.at>
2026-01-30 16:39:45 +0100

    Improve mathematical correctness part of spec

--- Curve25519Dalek/Specs/Ristretto/CompressedRistretto/Decompress.lean
+++ Curve25519Dalek/Specs/Ristretto/CompressedRistretto/Decompress.lean
@@ -1,9 +1,10 @@
 /-
-Copyright (c) 2025 Beneficial AI Foundation. All rights reserved.
+Copyright (c) 2026 Beneficial AI Foundation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Markus Dablander
 -/
 import Curve25519Dalek.Funs
+import Curve25519Dalek.Defs
 import Curve25519Dalek.Defs.Edwards.Representation
 
 /-! # Spec Theorem for `CompressedRistretto::decompress`
@@ -38,28 +39,31 @@ natural language specs:
 
 - The function always succeeds for all U8x32 input arrays (no panic)
 - If the input is not valid, then the output is none
-- If the input is valid, then the output is a valid Ristretto point that, when compressed again,
-  returns the original input.
+- If the input is valid, then the output is a valid Ristretto point that reflects the
+  output of the pure mathematical decompression function
 -/
 
 /-- **Spec and proof concerning `ristretto.CompressedRistretto.decompress`**:
 - The function always succeeds for all U8x32 input arrays (no panic)
 - If the input is not valid, then the output is none
-- If the input is valid, then the output is a valid Ristretto point that, when compressed again,
-  returns the original input.
+- If the input is valid, then the output is a valid Ristretto point that reflects the
+  output of the pure mathematical decompression function
 -/
 theorem decompress_spec (comp : CompressedRistretto) :
 
-  ∃ result, decompress comp = ok result ∧
+    ∃ result, decompress comp = ok result ∧
 
-  (¬comp.IsValid →
-    result = none) ∧
+    (¬comp.IsValid →
+      result = none) ∧
 
-  (comp.IsValid →
-      ∃ rist,
-      result = some rist ∧
-      RistrettoPoint.IsValid rist ∧
-      RistrettoPoint.compress rist = ok comp) := by
+    (comp.IsValid →
+        ∃ rist,
+        result = some rist ∧
+        RistrettoPoint.IsValid rist ∧
+        math.decompress_pure (U8x32_as_Nat comp) = some rist.toPoint)
+
+        := by
+
   sorry
 
 end curve25519_dalek.ristretto.CompressedRistretto
