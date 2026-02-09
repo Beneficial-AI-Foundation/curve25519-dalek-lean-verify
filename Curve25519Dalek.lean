d1dd663e01c2005c4d75133521e46ca516f49c84
a-dangelo <dangelo.alessand@gmail.com>
2026-02-09 22:38:45 +0000

    chore: update imports via mk_all

--- Curve25519Dalek.lean
+++ Curve25519Dalek.lean
@@ -1,12 +1,12 @@
 import Curve25519Dalek.Aux
+import Curve25519Dalek.Funs
+import Curve25519Dalek.FunsExternal
 import Curve25519Dalek.Math.Basic
 import Curve25519Dalek.Math.Edwards.Curve
 import Curve25519Dalek.Math.Edwards.Representation
-import Curve25519Dalek.Math.Ristretto.Representation
 import Curve25519Dalek.Math.Montgomery.Curve
 import Curve25519Dalek.Math.Montgomery.Representation
-import Curve25519Dalek.Funs
-import Curve25519Dalek.FunsExternal
+import Curve25519Dalek.Math.Ristretto.Representation
 import Curve25519Dalek.Specs.Backend.Serial.CurveModels.AffineNielsPoint.Add
 import Curve25519Dalek.Specs.Backend.Serial.CurveModels.AffineNielsPoint.AssertReceiverIsTotalEq
 import Curve25519Dalek.Specs.Backend.Serial.CurveModels.AffineNielsPoint.ConditionalAssign
