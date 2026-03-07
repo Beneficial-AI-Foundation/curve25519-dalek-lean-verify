c5cf8b3d2e38c803184fd495e6d3744c9d4cb722
a-dangelo <dangelo.alessand@gmail.com>
2026-03-07 16:39:49 +0100

    chore: remove scratch file

--- Scratch.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib
-
-attribute [grind =] Nat.add_mod Nat.mul_mod_mul_left Nat.mod_eq_of_lt
-attribute [grind .] Nat.mod_lt
-
-example (m n : Nat) (hn : 0 < n) :
-    (1 + 2 * m) % (2 * n) = 1 + 2 * (m % n) := by
-    grind 
