ec7f751e8a598b68ff8cea7eefa03a800a5ab539
Zhang-Liao <zhangliao714@gmail.com>
2026-03-24 17:00:47 +0800

    add missing copyrights info

--- Curve25519Dalek/Specs/Backend/Serial/U64/Field/FieldElement51/Reduce.lean
+++ Curve25519Dalek/Specs/Backend/Serial/U64/Field/FieldElement51/Reduce.lean
@@ -1,3 +1,8 @@
+/-
+Copyright 2026 The Beneficial AI Foundation. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Oliver Butterley, Theodore Ehrenborg, Alessandro D'Angelo, Liao Zhang
+-/
 import Curve25519Dalek.Math.Basic
 import Curve25519Dalek.Funs
 import Mathlib.Tactic.IntervalCases
