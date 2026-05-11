15d0e95b262380222c97444c269da4b09408f953
Zhang-Liao <zhangliao714@gmail.com>
2026-05-11 22:54:06 +0800

    save

--- Curve25519Dalek/Specs/Backend/StrausMultiscalarMul.lean
+++ Curve25519Dalek/Specs/Backend/StrausMultiscalarMul.lean
@@ -6,8 +6,7 @@ Authors: Oliver Butterley, Alessandro D'Angelo, Liao Zhang
 import Curve25519Dalek.Funs
 import Curve25519Dalek.Math.Basic
 
-/-!
-# Spec theorem for `curve25519_dalek::backend::straus_multiscalar_mul`
+/-! # Spec theorem for `curve25519_dalek::backend::straus_multiscalar_mul`
 
 Performs a multi-scalar multiplication of the form `∑ᵢ sᵢ · Pᵢ` on the Edwards
 curve using Straus' algorithm.
@@ -16,7 +15,6 @@ Source: "curve25519-dalek/src/backend/mod.rs"
 -/
 
 open Aeneas Aeneas.Std Result Aeneas.Std.WP
-
 namespace curve25519_dalek.backend
 
 -- Specification theorem to be written here
